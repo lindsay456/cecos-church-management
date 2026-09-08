f8d21bc909bb3d2dceaee82828f4c328ebc5dcf7
lindsay30-dev <sarah.boyom@saintjeaningenieur.org>
2026-09-08 10:53:36 +0200

    chore: remove temp scripts from tracking

--- auth_login.ps1
+++
@@ -1,3 +0,0 @@
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-gh auth login -h github.com -p https -w 2>&1 | Tee-Object -FilePath "C:\Users\HP PROBOOK\Documents\Default Project\gh_output.txt"
-gh auth status 2>&1 | Tee-Object -FilePath "C:\Users\HP PROBOOK\Documents\Default Project\gh_output.txt" -Append
